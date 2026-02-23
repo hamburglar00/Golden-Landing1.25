887a2772e447ad8bdb28759ab1528c1f34ff4a36
hamburglar00 <hamburglar002@proton.me>
2026-02-23 10:37:07 -0300

    Update google-sheets.js

--- credenciales/google-sheets.js
+++ credenciales/google-sheets.js
@@ -1,3 +1,3 @@
 export const CONFIG_SHEETS = {
-  GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/TU_APPS_SCRIPT_ID/exec'
+  GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycbxQT9GA6-N5I7NzxuLgCymyaLna7WYoO1wTLm3PYHAh_GW3NtFmhK8SWCxJKxudQ3aSTA/exec'
 };
